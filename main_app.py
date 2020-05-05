7241a8a11c7c459667739a0c8b322293525b81b2
Matthew Kinzler <matthew.kinzler@gmail.com>
2020-05-05 19:08:49 -0400

    add code design doc, add first test

---
+++ main_app.py
@@ -0,0 +1,4 @@
+# beer tasting app for thanksgiving beer tasting event
+
+def do_something():
+    return "I did something"
--- test_main_app.py
+++ test_main_app.py
@@ -1,5 +1,6 @@
-import main_app
+from main_app import do_something
 
 class TestMainApp:
-    def test_something(self):
-        pass
+    def test_do_something(self):
+        dut = do_something()
+        assert dut == "I did something"
